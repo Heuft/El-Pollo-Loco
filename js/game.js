9b3ca694e184b45c97cbcffff4de2b4c3f192575
Heuft <max.heuft@rocketmail.com>
2025-04-01 10:30:39 +0200

    Implement Character and Chicken

--- js/game.js
+++ js/game.js
@@ -1,11 +1,10 @@
 let canvas;
-let ctx;
-let character = new Character();
-let enemies = [new Chicken(), new Chicken(), new Chicken()];
+let world;
 
 function init() {
   canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
+  world = new World(canvas);
 
-  console.log("My Character is", character);
+  console.log("My Character is", world.character);
+  console.log("My Enemies is", world.enemies);
 }
