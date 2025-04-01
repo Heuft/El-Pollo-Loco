d33c58498208453af4f7f4f9180309bf117e4318
Heuft <max.heuft@rocketmail.com>
2025-04-01 23:34:50 +0200

    Implemented Clouds

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,15 +1,18 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
-
+  clouds = [new Cloud()];
+  canvas;
   ctx;
 
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
     this.draw();
   }
 
   draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.drawImage(
       this.character.img,
       this.character.x,
@@ -17,5 +20,28 @@ class World {
       this.character.width,
       this.character.height
     );
+
+    this.enemies.forEach((enemy) => {
+      this.ctx.drawImage(
+        enemy.img,
+        enemy.x,
+        enemy.y,
+        enemy.width,
+        enemy.height
+      );
+    });
+    this.clouds.forEach((cloud) => {
+      this.ctx.drawImage(
+        cloud.img,
+        cloud.x,
+        cloud.y,
+        cloud.width,
+        cloud.height
+      );
+    });
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
   }
 }
