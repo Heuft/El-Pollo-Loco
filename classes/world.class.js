3b44e87c5c6de670ffc462e3271ab9a3a943a02a
Heuft <max.heuft@rocketmail.com>
2025-04-02 17:42:02 +0200

    add BG + Skale Enemy and Character

--- classes/world.class.js
+++ classes/world.class.js
@@ -2,6 +2,12 @@ class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud()];
+  backgroundObject = [
+    new BackgroundObject("../img/5_background/layers/air.png", 0),
+    new BackgroundObject("../img/5_background/layers/3_third_layer/1.png", 0),
+    new BackgroundObject("../img/5_background/layers/2_second_layer/1.png", 0),
+    new BackgroundObject("../img/5_background/layers/1_first_layer/1.png", 0),
+  ];
   canvas;
   ctx;
 
@@ -13,35 +19,24 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.drawImage(
-      this.character.img,
-      this.character.x,
-      this.character.y,
-      this.character.width,
-      this.character.height
-    );
+    this.addObjectToMap(this.backgroundObject);
+    this.addToMap(this.character);
+    this.addObjectToMap(this.clouds);
+    this.addObjectToMap(this.enemies);
 
-    this.enemies.forEach((enemy) => {
-      this.ctx.drawImage(
-        enemy.img,
-        enemy.x,
-        enemy.y,
-        enemy.width,
-        enemy.height
-      );
-    });
-    this.clouds.forEach((cloud) => {
-      this.ctx.drawImage(
-        cloud.img,
-        cloud.x,
-        cloud.y,
-        cloud.width,
-        cloud.height
-      );
-    });
     let self = this;
     requestAnimationFrame(function () {
       self.draw();
     });
+  }
+
+  addObjectToMap(objects) {
+    objects.forEach((o) => {
+      this.addToMap(o);
+    });
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }
