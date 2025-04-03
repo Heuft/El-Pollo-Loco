e3d838515ac1ba7161eaa84d59411f02defe6bb6
Heuft <max.heuft@rocketmail.com>
2025-04-03 15:56:12 +0200

    Character Animate

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,8 +1,29 @@
 class Character extends MovableObject {
-  height = 300;
-  y = 135;
+  height = 200;
+  y = 225;
+  Images_Walking = [
+    "../img/2_character_pepe/2_walk/W-21.png",
+    "../img/2_character_pepe/2_walk/W-22.png",
+    "../img/2_character_pepe/2_walk/W-23.png",
+    "../img/2_character_pepe/2_walk/W-24.png",
+    "../img/2_character_pepe/2_walk/W-25.png",
+    "../img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
   constructor() {
     super().loadImage("../img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.Images_Walking);
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.Images_Walking.length;
+      let path = this.Images_Walking[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100);
   }
   jump() {}
 }
--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,10 +1,17 @@
 class Cloud extends MovableObject {
-  x = Math.random() * 500;
-  y = 20;
+  y = 50;
   width = 500;
+  height = 250;
   constructor() {
     super().loadImage("../img/5_background/layers/4_clouds/1.png");
 
-    this.height = 250;
+    this.x = Math.random() * 500;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.15;
+    }, 1000 / 60);
   }
 }
